6f4b78c73ef8e198dde01de0909afc00af626d14
siinn <che.32@cadence8.mps.ohio-state.edu>
2018-01-10 15:48:06 -0500

    Added jobOptions

--- share/grid_submit_data.py
+++ share/grid_submit_data.py
@@ -1,7 +1,8 @@
 #!/bin/sh
-pathena FlibBkgEst_jobOption_data.py \
+pathena DispDilepAthenaAlgJobOptions_data.py \
 --mergeOutput \
---destSE BNL-OSG2_USERDISK \
---nFilesPerJob 1 \
+--destSE BNL-OSG2_SCRATCHDISK \
+--nFilesPerJob 10 \
+--forceStaged \
 --inDS user.sche.data16_13TeV.physics_Main.DAOD_SUSY15.r8669_p2950.Lep_Filtered.r10_EXT0/ \
---outDS user.sche.data16_13TeV.physics_Main.DAOD_SUSY15.r8669.p2950.Lep_Filtered.FBE.r11
+--outDS user.sche.data16_13TeV.physics_Main.DAOD_SUSY15.r8669.p2950.Lep_Filtered.TF.data.z0d0Refitting.r73
